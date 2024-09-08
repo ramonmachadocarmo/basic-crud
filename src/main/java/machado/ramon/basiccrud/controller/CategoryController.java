cc1e232427b61ffe46b70e9564590af8959979f3
Ramon <ramon@prmord.io>
2024-09-08 13:39:34 -0400

    feature[WIP]: get & get by id

--- src/main/java/machado/ramon/basiccrud/controller/CategoryController.java
+++ src/main/java/machado/ramon/basiccrud/controller/CategoryController.java
@@ -1,7 +1,9 @@
 package machado.ramon.basiccrud.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +11,9 @@ import machado.ramon.basiccrud.model.Category;
 import machado.ramon.basiccrud.service.CategoryService;
 
 import java.util.List;
+import java.util.UUID;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/category")
@@ -24,4 +29,23 @@ public class CategoryController {
   public ResponseEntity<List<Category>> findAll() {
     return ResponseEntity.ok().body(categoryService.findAll());
   }
+
+  @GetMapping("/findById/{id}")
+  public ResponseEntity<Category> findById(@PathVariable UUID id) {
+    try {
+      return ResponseEntity.ok().body(categoryService.findById(id));
+    } catch (RuntimeException e) {
+      return ResponseEntity.notFound().build();
+    }
+  }
+
+  @PostMapping("/create")
+  public ResponseEntity<Object> create(@RequestBody Category category) {
+    try{
+      return ResponseEntity.ok().body(categoryService.create(category));
+    }catch(RuntimeException e){
+      return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+    }
+  }
+
 }
