81af5ef3466f7e73963efc64326f4552cf1242a0
Ramon <ramon@prmord.io>
2024-09-10 23:13:55 -0400

    hotfix: fixing env and configs

--- src/test/java/machado/ramon/basiccrud/ProductTests.java
+++ src/test/java/machado/ramon/basiccrud/ProductTests.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
