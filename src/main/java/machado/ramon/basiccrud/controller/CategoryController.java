b18f7b7a5104f8a359f68e057a9d5785d1006473
Ramon <ramon@prmord.io>
2024-09-10 22:57:48 -0400

    improvement: add pagination and order rules

--- src/main/java/machado/ramon/basiccrud/controller/CategoryController.java
+++ src/main/java/machado/ramon/basiccrud/controller/CategoryController.java
@@ -1,18 +1,21 @@
 package machado.ramon.basiccrud.controller;
 
 import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import machado.ramon.basiccrud.model.Category;
 import machado.ramon.basiccrud.model.CategoryDTO;
 import machado.ramon.basiccrud.service.CategoryService;
 
-import java.util.List;
 import java.util.UUID;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,8 +33,10 @@ public class CategoryController {
   }
 
   @GetMapping("/findAll")
-  public ResponseEntity<List<Category>> findAll() {
-    return ResponseEntity.ok().body(categoryService.findAll());
+  public ResponseEntity<Page<Category>> findAll(@RequestParam(value = "offset", defaultValue = "0") Integer offset,
+                                    @RequestParam(value = "pageSize", defaultValue = "10") Integer pageSize,
+                                    @RequestParam(value = "sortBy", defaultValue = "id") String sortBy) {
+    return ResponseEntity.ok().body(categoryService.findAll(PageRequest.of(offset, pageSize, Sort.by(sortBy))));
   }
 
   @GetMapping("/findById/{id}")
